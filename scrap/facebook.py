061eb63ae73185175fd55e1586874ed8d9eea479
rahmon-marcosse <rahmon.magalhaes@kepha.com.br>
2025-05-09 18:52:54 -0300

    Adding some files to use.

---
+++ scrap/facebook.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+
+
+
+class FacebookScraper:
+    @staticmethod
+    def login(driver: webdriver) -> webdriver:
+        pass
+
+    @staticmethod
+    def search_in_marketplace(driver: webdriver, search_string: str) -> webdriver:
+        pass
+
+    @staticmethod
+    def scroll_marketplace_to_bottom(driver: webdriver, search_string: str) -> webdriver:
+        pass
+
+    @staticmethod
+    def find_all_available_products(driver: webdriver) -> list:
+        pass
